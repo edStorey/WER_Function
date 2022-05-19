b9006c0bdd98be5e1af8320e41b122a9ee28b7b1
edStorey <estorey90@gmail.com>
2022-05-19 14:49:24 +0100

    added new function that also returns SID values

--- WER_Function.py
+++ WER_Function.py
@@ -11,20 +11,39 @@ N is the number of words in the reference (N=S+D+C)
 
 """
 
-preds = "Hello World good word"
-target = "Hello World goodbye world"
 
-Man_WER = (2 + 0 + 0)/ 4
+def main() :
+    preds = "Hey World goodbye world"
+    target = "World goodbye world"
 
-print("Manual Calculated WER: " + str(Man_WER))
+    S = 0
+    I = 0
+    D = 1
+    SID = S + I + D
+    C = 3
+    N = S + D + C
+
+    Man_WER = SID/ N
+
+    print("Manual Calculated WER: " + str(Man_WER) + ", Cor: " + str(C), ", Sub: " + str(S), ", Ins: " + str(I), ", Del: " + str(D))
+
+    P = preds.split()
+    T = target.split()
+
+    print(wer(P, T))
+
+    print(wer_ISD(preds, target))
 
 #def WER(preds, target) :
 #    P = preds.split()
- #   T = target.split()
+#   T = target.split()
 
 
 
 def wer(r, h):
+    """ Reference is the word generated from the ASR AI
+        Hypothesis is the original text.
+    """
     """
     Calculation of WER with Levenshtein distance.
 
@@ -79,9 +98,116 @@ def wer(r, h):
 
 
 
+def wer_ISD(ref, hyp ,debug=False):
+    """ Reference is the word generated from the ASR AI
+        Hypothesis is the original text.
+    """
+
+    r = ref.split()
+    h = hyp.split()
+    #costs will holds the costs, like in the Levenshtein distance algorithm
+    costs = [[0 for inner in range(len(h)+1)] for outer in range(len(r)+1)]
+    # backtrace will hold the operations we've done.
+    # so we could later backtrace, like the WER algorithm requires us to.
+    backtrace = [[0 for inner in range(len(h)+1)] for outer in range(len(r)+1)]
+ 
+    OP_OK = 0
+    OP_SUB = 1
+    OP_INS = 2
+    OP_DEL = 3
+
+    Penalty = 1
+    DEL_PENALTY = Penalty
+    INS_PENALTY = Penalty
+    SUB_PENALTY = Penalty 
+     
+    # First column represents the case where we achieve zero
+    # hypothesis words by deleting all reference words.
+    for i in range(1, len(r)+1):
+        costs[i][0] = DEL_PENALTY*i
+        backtrace[i][0] = OP_DEL
+         
+    # First row represents the case where we achieve the hypothesis
+    # by inserting all hypothesis words into a zero-length reference.
+    for j in range(1, len(h) + 1):
+        costs[0][j] = INS_PENALTY * j
+        backtrace[0][j] = OP_INS
+     
+    # computation
+    for i in range(1, len(r)+1):
+        for j in range(1, len(h)+1):
+            if r[i-1] == h[j-1]:
+                costs[i][j] = costs[i-1][j-1]
+                backtrace[i][j] = OP_OK
+            else:
+                substitutionCost = costs[i-1][j-1] + SUB_PENALTY # penalty is always 1
+                insertionCost    = costs[i][j-1] + INS_PENALTY   # penalty is always 1
+                deletionCost     = costs[i-1][j] + DEL_PENALTY   # penalty is always 1
+                 
+                costs[i][j] = min(substitutionCost, insertionCost, deletionCost)
+                if costs[i][j] == substitutionCost:
+                    backtrace[i][j] = OP_SUB
+                elif costs[i][j] == insertionCost:
+                    backtrace[i][j] = OP_INS
+                else:
+                    backtrace[i][j] = OP_DEL
+                 
+    # back trace though the best route:
+    i = len(r)
+    j = len(h)
+    Sub = 0
+    Del = 0
+    Ins = 0
+    Cor = 0
+    if debug:
+        print("OP\tREF\tHYP")
+        lines = []
+    while i > 0 or j > 0:
+        if backtrace[i][j] == OP_OK:
+            Cor += 1
+            i-=1
+            j-=1
+            if debug:
+                lines.append("OK\t" + r[i]+"\t"+h[j])
+        elif backtrace[i][j] == OP_SUB:
+            Sub +=1
+            i-=1
+            j-=1
+            if debug:
+                lines.append("SUB\t" + r[i]+"\t"+h[j])
+        elif backtrace[i][j] == OP_INS:
+            numIns += 1
+            j-=1
+            if debug:
+                lines.append("INS\t" + "****" + "\t" + h[j])
+        elif backtrace[i][j] == OP_DEL:
+            Del += 1
+            i-=1
+            if debug:
+                lines.append("DEL\t" + r[i]+"\t"+"****")
+    if debug:
+        lines = reversed(lines)
+        for line in lines:
+            print(line)
+        print("#cor " + str(Cor))
+        print("#sub " + str(Sub))
+        print("#del " + str(Del))
+        print("#ins " + str(Ins))
+    #return (numSub + numDel + numIns) / (float) (len(r))
+    WER_tot = (Sub + Del + Ins) / (float) (len(r))
+    return WER_tot, Cor, Sub, Ins, Del
 
 
-P = preds.split()
-T = target.split()
 
-print(wer(T, P))
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
